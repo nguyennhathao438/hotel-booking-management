3be5ac6fcb60a2250c90ba459aacb0254bbc42c3
Hao Nguyen <hao362k5@gmail.com>
2025-10-10 12:51:45 +0700

    setup env

--- backend/src/main/java/com/hotelbooking/hotel_booking/config/CloudinaryConfig.java
+++ backend/src/main/java/com/hotelbooking/hotel_booking/config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package com.hotelbooking.hotel_booking.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +10,20 @@ import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
     @Bean
     public Cloudinary configKey(){
         Map<String,String> config = new HashMap<>();
-        config.put("cloud_name","dm2gmucce");
-        config.put("api_key","768699196193148");
-        config.put("api_secret","-Z2iLQDQi1sUShgbPkKGrCHlz4I");
+        config.put("cloud_name",cloudName);
+        config.put("api_key",apiKey);
+        config.put("api_secret",apiSecret);
         return new Cloudinary(config);
     }
 }
